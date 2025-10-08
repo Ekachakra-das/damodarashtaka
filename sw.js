600a3ca19561cedc1d82747b566696fb04e04f67
Ekachakra <denisovskiizhenya@gmail.com>
2025-10-08 15:54:11 +0300

    update

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
-const CACHE_NAME = 'damodarashtakam-v1';
+const CACHE_NAME = 'damodarashtakam-v3';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js',
   '/verses.html',
+  '/verses-ru.html',
   '/manifest.json',
   '/img/1 Large.jpeg',
   '/img/2 Large.jpeg',
-  '/img/3 Large.jpeg'
+  '/img/3 Large.jpeg',
+  '/screenshots/1.jpeg',
+  '/screenshots/2.jpeg',
+  '/screenshots/3.jpeg',
+  '/screenshots/4.jpeg'
 ];
 
 self.addEventListener('install', event => {
@@ -21,6 +26,23 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // Clone the response
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
   );
 });
